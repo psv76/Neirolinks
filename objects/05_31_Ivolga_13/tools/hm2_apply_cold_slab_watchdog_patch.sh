812c0047983a909a2c706f50b289074e2a7d83b3
Сергей Перепёлкин <117675861+psv76@users.noreply.github.com>
2026-09-26 13:48:12 +0500

    Русская версия Excel-аудита Даниловского

--- objects/05_31_Ivolga_13/tools/hm2_apply_cold_slab_watchdog_patch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Retired: the v1 patch could hide lack of warmup for up to 24 hours.
-echo 'Deprecated: use the reviewed production cold-slab v2 installation procedure.' >&2
-echo 'See ../COLD_SLAB_WARMUP_V2.md. No files or outputs were changed.' >&2
-exit 2
